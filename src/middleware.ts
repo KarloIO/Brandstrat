5b3f855ee4f047e60510a76fd619ee248c9e1832
Karlo Aldrete <66887635+KarloAldrete@users.noreply.github.com>
2023-09-13 17:35:37 -0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,24 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
 
-export async function middleware(req: NextRequest) {
-    const res = NextResponse.next()
-    const supabase = createMiddlewareClient({ req, res })
-    const {
-        data: { session },
-    } = await supabase.auth.getSession()
+// export async function middleware(req: NextRequest) {
+//     const res = NextResponse.next()
+//     const supabase = createMiddlewareClient({ req, res })
+//     const {
+//         data: { session },
+//     } = await supabase.auth.getSession()
 
-    if (session?.user.email?.endsWith('.com')) {
-        return res
-    }
+//     if (session?.user.email?.endsWith('.com')) {
+//         return res
+//     }
 
-    const redirectUrl = req.nextUrl.clone()
-    redirectUrl.pathname = '/'
-    redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname)
-    return NextResponse.redirect(redirectUrl)
-}
+//     const redirectUrl = req.nextUrl.clone()
+//     redirectUrl.pathname = '/'
+//     redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname)
+//     return NextResponse.redirect(redirectUrl)
+// }
 
-export const config = {
-    matcher: '/middleware-protected/:path*',
-}
+// export const config = {
+//     matcher: '/middleware-protected/:path*',
+// }
