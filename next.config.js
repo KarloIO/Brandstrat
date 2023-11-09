f0b76098b1703eb3417add87d3b43e8021ea4539
Karlo Aldrete <karlo.aldretev1@gmail.com>
2023-11-09 10:55:30 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
   experimental: {
     webpackBuildWorker: true
   },
+  swcMinify: false,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(txt|jpg|gif|svg|eot|ttf|woff|woff2|mp3|ogg|wav)$/,
@@ -13,6 +14,14 @@ module.exports = {
         },
       },
     });
+
+    config.module.rules.push({
+      test: /node_modules\/langchain/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+
     return config;
   },
 }
